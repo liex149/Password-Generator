870e3c31a5708345835288d6f2274559a2f843d7
Linh Lien <lienx149@gmail.com>
2023-07-01 15:40:07 -0500

    created prompt for length of password

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,14 @@ function writePassword() {
   let password = "";
   let passwordText = document.querySelector("#password");
 
+  let pwlength = prompt("enter length here");
+  if (pwlength == null) { return; }
+  if (pwlength < 8 || pwlength > 128) {
+    alert("Please select between 8-128 characters");
+    writePassword();
+  }
+
+  
 }
 
 // Add event listener to generate button
