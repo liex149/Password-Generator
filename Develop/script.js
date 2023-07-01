3632f11afb0440a555ec5e49bd74918256b35c6c
Linh Lien <lienx149@gmail.com>
2023-07-01 16:11:52 -0500

    added additional special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,11 @@
-// Assignment code here
+
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let numeric = "0123456789";
-let specialChar = "!@#$%^&*()";
+let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_{|}~";
 
-
-// Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   let password = "";
   let passwordText = document.querySelector("#password");
@@ -49,16 +46,12 @@ function writePassword() {
     writePassword();
   }
 
-
   passwordText.textContent = password[Math.floor(Math.random() * password.length)];
 
   for (let i = 0; i < pwlength; i++) {
     passwordText.textContent += password[Math.floor(Math.random() * password.length)];
-
-    
   }
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
